bd2a917bd7a6c55a6d082b0113c7de5cb2d0b680
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-12 08:16:37 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep9/app/AppInitializer.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep9.app;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
