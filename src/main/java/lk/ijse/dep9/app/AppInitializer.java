56eb46a99084cebc2e2f25197a3a14aec610be33
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-18 10:13:22 +0530

    Update student service impl logic

--- src/main/java/lk/ijse/dep9/app/AppInitializer.java
+++ src/main/java/lk/ijse/dep9/app/AppInitializer.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AppInitializer {
 
     public static void main(String[] args) {
--- src/main/java/lk/ijse/dep9/app/service/custom/impl/StudentServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/StudentServiceImpl.java
@@ -9,11 +9,11 @@ import lk.ijse.dep9.app.service.util.Transformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
-@Component
-@Transactional
+@Service
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
@@ -26,6 +26,7 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+    @Transactional
     public void createNewStudent(StudentDTO studentDTO) {
         Optional<Student> student = studentRepository.findById(studentDTO.getNic());
         if (student.isEmpty()) studentRepository.save(transformer.toStudentEntity(studentDTO));
@@ -33,6 +34,7 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+    @Transactional
     public StudentDTO getStudentInfo(String nic) {
         if (!studentRepository.existsById(nic)) throw new NotFoundException("Student doesn't exist");
         Optional<Student> student = studentRepository.findById(nic);
@@ -40,6 +42,7 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+    @Transactional
     public void updateStudentDetails(StudentDTO studentDTO) {
         Optional<Student> student = studentRepository.findById(studentDTO.getNic());
         if (student.isPresent()) studentRepository.save(transformer.toStudentEntity(studentDTO));
@@ -47,6 +50,7 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+    @Transactional
     public void deleteStudent(String nic) {
         if (!studentRepository.existsById(nic)) throw new NotFoundException("Student doesn't exist");
         studentRepository.deleteById(nic);
